0ffb00ad20a28f9f619976137e3391cbf972c30c
Andrea  Piazza <andrea@Andreas-MacBook-Pro.local>
2018-10-05 11:06:03 -0700

    updated User Model

--- models/users.js
+++ models/users.js
@@ -1,9 +1,18 @@
 var mongoose = require('../db/connection')
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    dateJoined: { type: Date, default: Date.now },
+    username: {
+        type: String,
+        required: true, 
+        unique: true
+    },
+    password: {
+        type: String, 
+        required: true,
+    },
+    dateJoined: { 
+        type: Date, 
+        default: Date.now },
     currentCity: String
 });
 
