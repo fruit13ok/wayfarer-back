8f1d12dda1daff5aa11123f0c168d0f60d29c9db
Andrea  Piazza <andrea@Andreas-MacBook-Pro.local>
2018-10-05 11:03:35 -0700

    updated db connection

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = Promise
 
-mongoose.connect('mongodb://localhost/walk-it-out')
+mongoose.connect('mongodb://localhost/wayfarer')
     .then(connection => console.log('Connection established'))
     .catch(err => console.log('Connection failed',err))
 
--- models/users.js
+++ models/users.js
@@ -3,8 +3,8 @@ var mongoose = require('../db/connection')
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String,
-    // dateJoined: { type: Date, default: Date.now },
-    // currentCity: String
+    dateJoined: { type: Date, default: Date.now },
+    currentCity: String
 });
 
 mongoose.model('User', UserSchema);
